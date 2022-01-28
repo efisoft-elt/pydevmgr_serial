d0a8120da0638d62cdce3ef291c19bd86a9fdbd9
Sylvain Guieu <sylvain.guieu@gmail.com>
2022-01-28 17:39:23 +0100

    pydevmgr_serial first commit
    this is now an individual package as part of the v0.3 code re-writting.

---
+++ pydevmgr_serial/serial_device.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, validator
+from typing import Optional, Union 
+from .serial_com import parse_com, SerialCom
+from pydevmgr_core import BaseDevice, reconfig
+
+
+
+class BaseSerialDeviceConfig(BaseDevice.Config):   
+    prefix: str = ""
+    # include the possibility to include an optional com configuration for standalone com
+    com: SerialCom.Config = SerialCom.Config()
+    
+    
+class BaseSerialDevice(BaseDevice):
+    Config = BaseSerialDeviceConfig
+    Com = SerialCom
+    def __init__(self, 
+           key: str, 
+           config: Optional[BaseSerialDeviceConfig] = None, 
+           com: Optional[SerialCom] = None, 
+           **kwargs
+        ) -> None:
+        # parse config and com 
+                 
+        super().__init__(key, config=config,  **kwargs) 
+        
+        self._com = parse_com(com, self._config.com)
+    
+    
+       
+    
+    @property
+    def com(self):
+        return self._com 
+    
+    def connect(self):
+        """connect the serial com """
+        self._com.connect()
+    
+    def disconnect(self):
+        """disconnect serial com """
+        self._com.disconnect()
+    
+    
---
+++ pydevmgr_serial/serial_node.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, validator
+from typing import Optional, Union 
+from .serial_com import parse_com, SerialCom 
+from pydevmgr_core import BaseNode, reconfig, kjoin
+
+
+
+class BaseSerialNodeConfig(BaseNode.Config):   
+    prefix: str = ""
+    # include the possibility to include an optional com configuration for standalone com
+    com: Optional[SerialCom.Config] = None
+    
+
+class BaseSerialNode(BaseNode):
+    Config = BaseSerialNodeConfig
+    Com = SerialCom
+    def __init__(self, key, config=None, com=None, **kwargs): 
+        # parse the config and com object 
+            
+        super().__init__(key, config=config, com=com, **kwargs)
+        self._com = parse_com(com, self._config.com)
+
+
+    @classmethod
+    def new_args(cls, parent, config):
+        d = super().new_args(parent, config)
+        d.update(com=parent.com)
+        return d
+
+    @property
+    def com(self):
+        return self._com 
+    
+    @property
+    def sid(self):
+        self._com.sid
+    
+    def connect(self):
+        self._com.connect()
+    
+    def disconnect(self):
+        self._com.disconnect()
+    
+    def fget(self):
+        raise NotImplementedError('fget')
+        
+    def fset(self, value):
+        raise NotImplementedError('fset')
+        
